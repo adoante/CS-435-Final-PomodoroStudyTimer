8a900b09ca0e50c4ab183d5875ccfaf30090f7ec
Adolfo Gante <adolfogante@gmail.com>
2024-11-28 17:10:32 -0800

    add: SwtichStatement, timers in own whileLoops

--- sketch_nov27b_copy_20241128153327.ino
+++ sketch_nov27b_copy_20241128153327.ino
@@ -11,22 +11,30 @@ Adafruit_SSD1306 oled(1);
 #define LOGO_HEIGHT   16
 #define LOGO_WIDTH    16
 
-// write displays
-void tcaselect(uint8_t i) {
-  if (i > 7) return;
-  Wire.beginTransmission(TCAADDR);
-  Wire.write(1 << i);
-  Wire.endTransmission(); 
-}
+// Variables
 
-const byte ledPin = 7;
-const byte interruptPin = 3;
-volatile byte state = LOW;
+// Adjustable by user
+volatile int study = 30;
+volatile int smallBreak = 30;
+volatile int longBreak = 30;
+volatile int interval = 4;
 
+// subtracted from and reset
+int studyTimer = study;
+int smallBreakTimer = smallBreak;
+int longBreakTimer = longBreak;
+int longBreakInterval = interval;
+
+// used to determine if long break is next
+int longBreakCounter = 0;
+
+// study = 0, break = 1, long break = 2
+int timer = 0;
+
+// setup - runs once
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);
+  // print to serial monitor
+  Serial.begin(9600);
 
   // oled setup
   oled.begin(SSD1306_SWITCHCAPVCC, OLED_Address);
@@ -44,91 +52,128 @@ void setup() {
   oled.display();
 }
 
-volatile int cnt = 300;
-volatile int studyTimer = 3000;
-volatile int smallBreakTimer = 0;
-volatile int longBreakInterval = 4;
-volatile int longBreakTimer = 900;
-
+// loops forever
 void loop() {
-  /*
-  tcaselect(2); // Text to Oled display 1
-  oled.begin();
-  oled.clearDisplay();
-  oled.setTextColor(WHITE);
-  oled.setCursor(0,0);
-  oled.setTextSize(3.5);
-  */
-
-  selectDisplay(2);
-
-  if(studyTimer == -1){
-    oled.print("0:00");
-  }
-  else {
-    oled.print(studyTimer/60);
-    oled.print(":");
-    oled.println(studyTimer%60);
-  }
-  //(cnt/60)+":"+(cnt%60)
-  if(studyTimer > 0){
-    studyTimer--;
-  }
   
-  oled.display();
+  switch(timer) {
+    // Study Timer case
+    case 0:
+      while (studyTimer >= 0) {
+        // display 1
+        selectDisplay(2, 0, 0, 3.5);
 
-  //Break timer
-  selectDisplay(3);
-  if(smallBreakTimer == -1){
-    oled.print("0:00");
-  }
-  else {
-    oled.print(smallBreakTimer/60);
-    oled.print(":");
-    oled.println(smallBreakTimer%60);
-  }
-  if(smallBreakTimer > 0){
-    smallBreakTimer--;
-  }
-  oled.display();
+        // print time
+        oled.print(studyTimer / 60);
+        oled.print(":");
+        oled.println(studyTimer % 60);
 
-  //Long Break timer
-  selectDisplay(4);
-  if(smallBreakTimer == -1){
-    oled.print("0:00");
-  }
-  oled.display();
+        // actually show something on screen
+        oled.display();
 
+        // update studyTimer
+        studyTimer--;
+      }
+
+      // update longBreakIntervalCounter
+      longBreakCounter++;
+
+      // set timer value for next break type (either short break or long break)
+      if (longBreakCounter == longBreakInterval) {
+        Serial.println("Switching to Long Break Timer");
+        timer = 2;
+      }
+      else {
+        Serial.println("Switching to Short Break Timer");
+        timer = 1;
+      }
+
+      // reset timer for next time
+      studyTimer = study;
+
+      break;
+
+    // Short Break Timer case
+    case 1:
+      while (smallBreakTimer >= 0) {
+        // display 2
+        selectDisplay(3, 0, 0, 3.5);
+
+        // print time
+        oled.print(smallBreakTimer/60);
+        oled.print(":");
+        oled.println(smallBreakTimer%60);
+
+        // actually show something on screen
+        oled.display();
+
+        // update smallBreakTimer
+        smallBreakTimer--;
+      }
+    
+      // set timer value for next studyTimer
+      timer = 0;
+      Serial.println("Switching to Study Timer");
+
+      // reset timer for next time
+      smallBreakTimer = smallBreak;
+
+      break;
+
+    // Long Break Timer
+    case 2:
+      while (longBreakTimer >= 0) {
+        // display 3
+        selectDisplay(4, 0, 0, 3.5);
+
+        // print time
+        oled.print(longBreakTimer/60);
+        oled.print(":");
+        oled.println(longBreakTimer%60);
+
+        // actually show something on screen
+        oled.display();
+
+        // update longBreakTimer
+        longBreakTimer--;
+      }
+    
+      // set timer value for next studyTimer
+      timer = 0;
+      Serial.println("Switching to Study Timer");
+
+      // reset timer for next time
+      longBreakTimer = longBreak;
+
+      break;
+
+    default:
+      // Menu Code goes here
+      // Choose timer to adjust
+      // Start Timer
+      // Display timer times
+      break;
+  }
+      
+  // Delay to match 1 second
   delay(10);
-
-  if (longBreakInterval = 0) {
-    
-  }
-
-  if(studyTimer == 0)
-    {
-      smallBreakTimer = 3000;
-      studyTimer--;
-    }
-  if(smallBreakTimer == 0)
-    {
-      studyTimer = 3000;
-      smallBreakTimer--;
-    }
-    
 }
 
-void blink() {
-  state = !state;
-  digitalWrite(ledPin, state);
-  studyTimer++;
+// Functions
+
+// write displays
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission(); 
 }
 
-void selectDisplay(int display) {
+// select display 
+void selectDisplay(int display, int xCursor, int yCursor, float textSize) {
   tcaselect(display);
   oled.begin();
   oled.clearDisplay();
   oled.setTextColor(WHITE);
-  oled.setCursor(0,0);
-  oled.setTextSize(3.5);
+  oled.setCursor(xCursor,yCursor);
+  oled.setTextSize(textSize);
 }
